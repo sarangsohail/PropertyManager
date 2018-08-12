9206ce6bc48853518668171d4d8ee13c614567a5
Sarang Sohail <sarangsohail@gmail.com>
2018-08-13 00:59:32 +0100

    sending a message works

--- app/src/main/java/com/example/propertymanagment/ChatActivity.java
+++ app/src/main/java/com/example/propertymanagment/ChatActivity.java
@@ -1,21 +1,31 @@
 package com.example.propertymanagment;
 
 import android.content.Context;
+import android.media.Image;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toolbar;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
 import com.google.firebase.database.ValueEventListener;
 
 import org.w3c.dom.Text;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -24,9 +34,16 @@ public class ChatActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
 
+    private FirebaseAuth maAuth;
+
     private TextView mTitleView;
     private TextView lastSeenView;
 
+    private ImageButton mChatAddButton;
+    private ImageButton mSendButton;
+    private EditText mChatMessageView;
+
+    String mCurrentUserID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,7 +58,7 @@ public class ChatActivity extends AppCompatActivity {
         mUserChat = getIntent().getStringExtra("user_id");
 
         //setting the actionbar to the tenant's/friend's name
-        String user_chat_name = getIntent().getStringExtra("user_name");
+        final String user_chat_name = getIntent().getStringExtra("user_name");
 //        getSupportActionBar().setTitle(user_chat_name);
 
         LayoutInflater layoutInflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -53,6 +70,14 @@ public class ChatActivity extends AppCompatActivity {
         mTitleView = (TextView) findViewById(R.id.custom_chat_name);
         lastSeenView = (TextView) findViewById(R.id.custom_bar_seen);
 
+        mChatAddButton = (ImageButton) findViewById(R.id.chat_add_btn);
+        mSendButton = (ImageButton) findViewById(R.id.chat_send_btn);
+        mChatMessageView = (EditText) findViewById(R.id.chat_message_view);
+
+
+
+        maAuth = FirebaseAuth.getInstance();
+        mCurrentUserID = maAuth.getUid();
         mRootRef = FirebaseDatabase.getInstance().getReference();
 
         mTitleView.setText(user_chat_name);
@@ -88,7 +113,92 @@ public class ChatActivity extends AppCompatActivity {
         });
 
 
+        mRootRef.child("Chat").child(mCurrentUserID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
 
+                if (!dataSnapshot.hasChild(mUserChat)){
+
+                    // values i want to be stored in the chat
+                    Map chatAddMap = new HashMap<>();
+                    chatAddMap.put("seen", false);
+                    chatAddMap.put("timestamp", ServerValue.TIMESTAMP);
+
+                    // storing the map above in both mine and the tenant's chat
+                    Map chatUserMap = new HashMap();
+                    chatUserMap.put("Chat/" + mCurrentUserID + "/" + mUserChat, chatAddMap);
+                    chatUserMap.put("Chat/" + mUserChat + "/" + mCurrentUserID, chatAddMap);
+
+                    mRootRef.updateChildren(chatUserMap, new DatabaseReference.CompletionListener() {
+                        @Override
+                        public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+
+                            Log.d("CHAT LOG  -----------", databaseError.getMessage().toString());
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+        //onClickListens for the chat view views
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMessage();
+            }
+
+            private void sendMessage() {
+
+                String message = mChatMessageView.getText().toString();
+
+                if (!TextUtils.isEmpty(message)){
+
+                    String currentUserRef = "messages/" + mCurrentUserID + "/" + mUserChat;
+                    String chat_user_ref = "messages/" + mUserChat + "/" + mCurrentUserID;
+
+                    //pushId for the message
+                    DatabaseReference user_message_push = mRootRef.child("messages/").child(mCurrentUserID)
+                            .child(mUserChat).push();
+
+                    String push_id = user_message_push.getKey();
+
+                    //information for the message being sent
+                    Map messageMap = new HashMap();
+                    messageMap.put("message", message);
+                    messageMap.put("seen", false);
+                    messageMap.put("type", "text");
+                    messageMap.put("time", ServerValue.TIMESTAMP);
+                    messageMap.put("from", mCurrentUserID);
+
+
+                    Map messageUserMap = new HashMap();
+                    messageUserMap.put(currentUserRef + "/" + push_id, messageMap);
+                    messageUserMap.put(chat_user_ref + "/" + push_id, messageMap);
+
+                    mRootRef.updateChildren(messageUserMap, new DatabaseReference.CompletionListener() {
+                        @Override
+                        public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+
+                            if (databaseError != null){
+                                Log.d("CHAT_LOG", databaseError.getMessage().toString());
+
+
+                            }
+                        }
+                    });
+
+
+
+                }
+
+            }
+        });
 
     }
 }
