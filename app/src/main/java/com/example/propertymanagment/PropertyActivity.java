d21e659c14b55b5e15db57beb84ec5ffb2f75bbf
Sarang Sohail <sarangsohail@gmail.com>
2018-08-24 15:18:05 +0100

    ui for the property addition

--- app/src/main/java/com/example/propertymanagment/PropertyActivity.java
+++ app/src/main/java/com/example/propertymanagment/PropertyActivity.java
@@ -2,19 +2,20 @@ package com.example.propertymanagment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 public class PropertyActivity extends AppCompatActivity {
 
-    private TextView mPropertyName;
+    private EditText mPropertyName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_property);
 
         getSupportActionBar().setTitle("Add Property");
-        mPropertyName = (TextView) findViewById(R.id.propertyNameTV);
+        mPropertyName = (EditText) findViewById(R.id.prop_street_et);
     }
 }
