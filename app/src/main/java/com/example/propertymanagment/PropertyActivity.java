f7037e09f0a9061d69a5887ce97350df981251c0
Sarang Sohail <sarangsohail@gmail.com>
2018-08-25 07:33:13 +0100

    additions to property adding UI

--- app/src/main/java/com/example/propertymanagment/PropertyActivity.java
+++ app/src/main/java/com/example/propertymanagment/PropertyActivity.java
@@ -2,7 +2,9 @@ package com.example.propertymanagment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -17,5 +19,21 @@ public class PropertyActivity extends AppCompatActivity {
 
         getSupportActionBar().setTitle("Add Property");
         mPropertyName = (EditText) findViewById(R.id.prop_street_et);
+
+        //all the spinners for the 'add property' class
+
+        Spinner paymentSpinner = (Spinner) findViewById(R.id.payment_frequency_spinner);
+        Spinner bedroomSpinner = (Spinner) findViewById(R.id.bedroom_spinner_add_prop);
+        ArrayAdapter<CharSequence> paymentSpinnerAdapter = ArrayAdapter.createFromResource(this,
+                R.array.payment_frequency_array_add_prop, android.R.layout.simple_spinner_item);
+        paymentSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        paymentSpinner.setAdapter(paymentSpinnerAdapter);
+
+
+        ArrayAdapter<CharSequence> bedroomSpinnerAdapter = ArrayAdapter.createFromResource(this,
+                R.array.bedroom_add_prop, android.R.layout.simple_spinner_item);
+        bedroomSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        bedroomSpinner.setAdapter(bedroomSpinnerAdapter);
+
     }
 }
