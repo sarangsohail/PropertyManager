b9d8a651c397df4b8a5a1a26dcf1545c76ecffde
Sarang Sohail <sarangsohail@gmail.com>
2018-08-30 21:31:12 +0100

    user profile information popup

--- app/src/main/java/com/example/propertymanagment/MainActivity.java
+++ app/src/main/java/com/example/propertymanagment/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.propertymanagment;
 
 import android.app.AlertDialog;
 import android.app.Application;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +12,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,6 +29,11 @@ public class MainActivity extends AppCompatActivity  {
 
     private CardView mProperty, mFinance, mTenant, mChat, mEmergency;
 
+    private Dialog aboutUserMenuDialoag;
+
+    private Button btnAaoutUserMenuDialoag;
+
+    private TextView about_user_main_textview_menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -139,7 +146,22 @@ public class MainActivity extends AppCompatActivity  {
             sendToStartActivity();
         }else if (item.getItemId() == R.id.about_user_button){
 
-            
+            String userEmail = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+
+            aboutUserMenuDialoag = new Dialog(this);
+            aboutUserMenuDialoag.setContentView(R.layout.about_popup_main);
+
+            about_user_main_textview_menu = (TextView) findViewById(R.id.about_user_main_textview);
+            //about_user_main_textview_menu.setText("Email: " );
+
+            btnAaoutUserMenuDialoag = (Button) findViewById(R.id.btn_ok_about_menu);
+
+            btnAaoutUserMenuDialoag.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    aboutUserMenuDialoag.dismiss();
+                }
+            });
         }
 
         return super.onOptionsItemSelected(item);
