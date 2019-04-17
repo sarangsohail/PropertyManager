dc10987a7bf4d663ce4f70a7763ce24b41a0bb59
Sarang Sohail <sarangsohail@gmail.com>
2019-04-17 13:45:24 +0100

    streaming locally works

--- app/src/main/java/com/example/propertymanagment/video_streaming_activity.java
+++ app/src/main/java/com/example/propertymanagment/video_streaming_activity.java
@@ -2,6 +2,9 @@ package com.example.propertymanagment;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class video_streaming_activity extends Activity {
 
@@ -9,7 +12,14 @@ public class video_streaming_activity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_streaming_activity);
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("http://192.168.1.7:8160/index.html");
+
     }
 
-
+//add on backpressed method and sort the navigation out between this activity and the rest of the app
 }
