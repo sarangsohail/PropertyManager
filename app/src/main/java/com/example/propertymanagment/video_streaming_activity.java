9806aad2ef0ee500bf550a64d3c5c7d94de51090
Sarang Sohail <sarangsohail@gmail.com>
2019-04-18 19:05:51 +0100

    stream condition checking works. Just need to perform a test on this activity.

--- app/src/main/java/com/example/propertymanagment/video_streaming_activity.java
+++ app/src/main/java/com/example/propertymanagment/video_streaming_activity.java
@@ -1,10 +1,22 @@
 package com.example.propertymanagment;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class video_streaming_activity extends Activity {
 
@@ -13,13 +25,35 @@ public class video_streaming_activity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_streaming_activity);
 
-        WebView myWebView = (WebView) findViewById(R.id.webview);
-        WebSettings webSettings = myWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        myWebView.setWebViewClient(new WebViewClient());
-        myWebView.loadUrl("http://192.168.1.7:8160/index.html");
+        try {
+            if (isNetworkAvailable()){
 
+                WebView streamView = (WebView) findViewById(R.id.webview);
+                WebSettings webSettings = streamView.getSettings();
+                webSettings.setJavaScriptEnabled(true);
+                streamView.setWebViewClient(new WebViewClient());
+                String url = "http://google.com";
+                //   myWebView.loadUrl("http://192.168.1.7:8160/index.html");
+                Toast.makeText(this, "Loading Stream", Toast.LENGTH_SHORT).show();
+                streamView.loadUrl(url);
+
+            }else{
+                Toast.makeText(this, "Problem loading your stream, try connecting to the internet", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+//        myWebView.setWebViewClient(new WebViewClient());
+//        myWebView.loadUrl("http://192.168.1.7:8160/index.html");
     }
 
-//add on backpressed method and sort the navigation out between this activity and the rest of the app
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting();    }
 }
+
